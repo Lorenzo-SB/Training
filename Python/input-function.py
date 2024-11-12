ce1b75804e28234333f1176ab5d04d682ec745cf
Lorenzo <lorenzo.lenzenhuber@storz-bickel.com>
2024-11-12 15:12:46 +0100

    input-function

---
+++ Python/input-function.py
@@ -0,0 +1,7 @@
+name = input("Wie ist dein Name?")
+age = int(input("Wie alt bist du?"))
+height = float(input("Wie groß bist du?"))
+
+print("Hallo "+name)
+print("Du bist "+str(age)+" Jahre alt")
+print("und "+str(height)+" cm groß.")
