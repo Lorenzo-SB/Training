6d48842fc5604ae1c45b47d8ff9065b05dfde1cd
Lorenzo <lorenzo.lenzenhuber@storz-bickel.com>
2024-11-12 16:46:53 +0100

    for-clause additions

--- Python/for-clause.py
+++ Python/for-clause.py
@@ -6,3 +6,23 @@ for i in range (rows):
     for j in range(columns):
         print(symbol, end="")
     print()
+
+# --------------------------
+
+phone_number = "1--_-23-45_67-89-1"
+
+for i in phone_number:
+    if i == "-":
+        continue
+    elif i == "_":
+        continue
+    print(i, end="")
+
+# --------------------------
+
+for i in range(0,21):
+
+    if i == 13:
+        pass
+    else:
+        print(i)
