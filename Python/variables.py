d8cc59643d1dea9d43f001d304ca9a4c6e030575
Lorenzo <lorenzo.lenzenhuber@storz-bickel.com>
2024-11-12 15:02:54 +0100

    variables

---
+++ Python/variables.py
@@ -0,0 +1,6 @@
+first_name = "Lorenzo"
+middle_name = "Leon"
+last_name = "Lenzenhuber"
+full_name = first_name + " " + middle_name + " " + last_name
+print((full_name))
+print("Guten Tag, " + full_name)
